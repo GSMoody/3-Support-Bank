4865c9d3d1dcbbef515cf03a4cb35a3678df64a6
gmoody <gmoody2508@gmail.com>
2019-09-11 11:05:00 +0100

    Added functionality for each employeee total

--- reader.py
+++ reader.py
@@ -1,32 +1,25 @@
 import csv
+import sys
 
 option=input("Select '1' to list value in credit/debit for each employee. Select '2' to list all transactions for a given name")
 if option == '1':
-    results=[]
     with open('Transactions2014.csv') as file:
         i=0
         names={}
         file=csv.reader(file,delimiter=',')
         for line in file:
-            #print(file)
+            debtor=line[1]
+            creditor=line[2]
             if i != 0:
-                if line[1] not in names:
-                    names.append(line[1])
-                if line[2] not in names:
-                    names.append(line[2])
-            i = i +1
+                val = float(line[4])
+                if debtor in names.keys():
+                    names[debtor] = names[debtor] - val
+                else:
+                    names[debtor] = -val
+                if creditor in names.keys():
+                    names[creditor] = names[creditor] + val
+                else:
+                    names[creditor] = val
+            i = i + 1
         print(names)
-        for name in names:
-            print(name)
-            debt=0.0
-            credit=0.0
-            #print(file)
-            for line in file:
-                print(line)
-                if name == line[1]:
-                    debt=debt+float(line[4])
-                elif name == line[2]:
-                    credit=credit+float(line[4])
-            sum=credit-debt
-            #print(name+"'s total balance is £"+str(sum))
 
