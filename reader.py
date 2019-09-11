a91768b864823ded071f7d1cbd6ecc2182cd8c15
gmoody <gmoody2508@gmail.com>
2019-09-11 12:01:23 +0100

    Tidied up data output, included rounding function

--- reader.py
+++ reader.py
@@ -3,6 +3,13 @@ import sys
 
 option=input("Select '1' to list value in credit/debit for each employee. Select '2' to list all transactions for a given name")
 
+def rounder(number):
+    number=round(number,2)
+    number=str(number)
+    number_split = number.split('.')
+    if len(number_split[1]) != 2:
+        number = number + '0'
+    return number
 
 def sum_all(file):
     i = 0
@@ -22,6 +29,10 @@ def sum_all(file):
                 names[creditor] = val
         i = i + 1
     print(names)
+    for name in names:
+        final_val=names[name]
+        final_val=rounder(final_val)
+        print("Account holder "+name+" has a total balance of £"+final_val)
 
 
 with open('Transactions2014.csv') as file:
@@ -37,7 +48,10 @@ with open('Transactions2014.csv') as file:
             creditor = line[2]
             if name == debtor or name == creditor:
                 account = True
-                print("Date "+line[0]+" Debtor: "+debtor+" Creditor: "+creditor+" Narrative: "+line[3]+" Value: £"+line[4])
+                number=line[4]
+                number=float(number)
+                number=rounder(number)
+                print("Date "+line[0]+" Debtor: "+debtor+" Creditor: "+creditor+" Narrative: "+line[3]+" Value: £"+number)
         if not account:
             print("Account does not exist!")
     else:
